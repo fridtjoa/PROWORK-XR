fcfce2c4cf7c6511cb4cc9b6a499a360c6580b50
mszabokass <m.szabokass@gmail.com>
2022-06-24 11:38:24 +0200

    Camera_follow2
    Nachbau1

---
+++ Assets/Scripts/GravityChanger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+    }
+}
